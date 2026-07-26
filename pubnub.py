9db3789d2f2f88ec8b9bda3033a23abc83cd6321
Stephen Blum <blum.stephen@gmail.com>
2026-07-26 08:41:25 -0700

    colors.

--- pubnub.py
+++ pubnub.py
@@ -1,17 +1,50 @@
 import requests
-import queue
-import Threading
+import threading
+from queue import Queue
 
 ## Transmit Player Movements
 def publish(channel, message):
-    pass
+    origin = 'h2.pubnubapi.com'
+    pubkey = 'demo'
+    subkey = 'demo'
+    payload = json.dumps(message)
+    uri = f'/publish/{pubkey}/{subkey}/0/{channel}/0/{payload}'
+
+    response = requests.get(uri)
+    return response.json()[0]
 
 ## Receive the game state
 def subscribe(channel, inbox):
-    origin = ''
-    timetoken = '1000'
-    subkey = 'demo'
-    uri=f"https://${origin}/subscribe/${subkey}/${channel}/0/${timetoken}";
-    response = requests.get(uri)
-    pass
-    
+    while True:
+        origin = 'h2.pubnubapi.com'
+        timetoken = '1000'
+        subkey = 'demo'
+        uri=f"https://{origin}/subscribe/{subkey}/{channel}/0/{timetoken}";
+        response = requests.get(uri)
+        data = response.json()
+        timetoken = data[1]
+        messages = data[0]
+
+        for message in messages:
+            inbox.put(message)
+
+def publisher(inbox, outbox):
+    while True:
+        message = inbox.get()
+        print(message)
+
+if __name__ == '__main__':
+    game_state_channel = 't-rex-dino-game-state'
+    game_move_channel = 't-rex-dino-game-movement'
+
+    ## Subscription Inbox
+    inbox = Queue()
+    subscription = threading.Thread(target=subscribe, args=(inbox,))
+
+    ## Publish Game Movement
+    outbox = Queue()
+    publishing = threading.Thread(target=publisher, args=(inbox, outbox,))
+
+    subscription.join()
+    publishing.join()
+
