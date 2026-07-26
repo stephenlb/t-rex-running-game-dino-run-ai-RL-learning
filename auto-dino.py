da1aaaeb6a2d3b183ec27f558248c14497719bea
Stephen Blum <blum.stephen@gmail.com>
2026-07-26 10:49:29 -0700

    state in py.

--- auto-dino.py
+++ auto-dino.py
@@ -5,19 +5,20 @@
 ## TODO 
 import pubnub
 import torch
+import threading
+from queue import Queue
 
-GAME_CHANNEL='t-rex-dino-game-state'
-game_state_channel = 't-rex-dino-game-state'
-game_move_channel = 't-rex-dino-game-movement'
+GAME_STATE_CHANNEL = 't-rex-dino-game-state'
+GAME_MOVE_CHANNEL = 't-rex-dino-game-movement'
 
 ## Subscription Inbox
 inbox = Queue()
-subscription = threading.Thread(target=subscribe, args=(game_state_channel, inbox,))
+subscription = threading.Thread(target=pubnub.subscribe, args=(GAME_STATE_CHANNEL, inbox,))
 subscription.start()
 
 ## Publish Game Movement
 outbox = Queue()
-publishing = threading.Thread(target=publisher, args=(inbox, outbox,))
+publishing = threading.Thread(target=pubnub.publisher, args=(inbox, outbox,))
 publishing.start()
 
 ## Join threads
